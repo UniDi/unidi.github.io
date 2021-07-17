53c00561695090b6ae60cbb23fcceecf19c62479
Mathijs Bakker <mathijs_bakker@me.com>
2021-07-17 10:50:42 +0200

    Update links to own repo.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,13 +17,13 @@ module.exports = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     editUrl:
-                    'https://github.com/facebook/docusaurus/edit/master/website/',
+                    'https://github.com/UniDi/unidi.github.io/edit/master/website/',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     editUrl:
-                    'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                    'https://github.com/UniDi/unidi.github.io/edit/master/website/blog/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
