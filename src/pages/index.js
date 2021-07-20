ac88719b7e931dcc7c22bf50ef204ade362b4c6f
Mathijs Bakker <mathijs_bakker@me.com>
2021-07-20 22:42:19 +0200

    Stylesheet, Components and Images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,14 @@ import Layout from '@theme/Layout';
 import CodeBlock from '@theme/CodeBlock';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+
 import HomepageFeatures from '../components/HomepageFeatures';
+import Section from '../components/Section';
+import LogoCarousel from '../components/LogoCarousel'
 import Highlight from '../components/Highlight';
 import UniDiLogoSVG from '../../static/img/unidi-logo.svg';
+
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -72,6 +76,55 @@ public class MageInstaller : Installer
 	}
 }
 `
+const logos = [
+	{
+		/**
+		 * Page 1
+		 */
+		img: '../../static/img/logos/macOS.svg',
+		alt: 'BuildTarget.StandaloneOSX',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.StandaloneOSX.html'
+	}, {
+		img: '../../static/img/logos/windows.svg',
+		alt: 'BuildTarget.StandaloneWindows',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.StandaloneWindows.html'
+	}, {
+		img: '../../static/img/logos/linux.svg',
+		alt: 'BuildTarget.StandaloneLinux64',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.StandaloneLinux64.html'
+	}, {
+		img: '../../static/img/logos/iOS.svg',
+		alt: 'BuildTarget.iOS',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.iOS.html'
+	}, {
+		img: '../../static/img/logos/android.svg',
+		alt: 'BuildTarget.Android',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.Android.html'
+	}, {
+		img: '../../static/img/logos/tvOS.svg',
+		alt: 'BuildTarget.tvOS',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.tvOS.html'
+	}, {
+		img: '../../static/img/logos/stadia.svg',
+		alt: 'BuildTarget.Stadia',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.Stadia.html'
+	}, {
+		img: '../../static/img/logos/playstation.svg',
+		alt: 'BuildTarget.PS4',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.PS4.html'
+	}, {
+		img: '../../static/img/logos/nintendo-switch.svg',
+		alt: 'BuildTarget.Switch',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.Switch.html'
+	}, {
+		img: '../../static/img/logos/xbox.svg',
+		alt: 'BuildTarget.XboxOne',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.XboxOne.html'
+	}, {
+		img: '../../static/img/logos/webgl.svg',
+		alt: 'BuildTarget.XboxOne',
+		url: 'https://docs.unity3d.com/ScriptReference/BuildTarget.WebGL.html'
+	}]
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -81,18 +134,35 @@ export default function Home() {
       description="Dependency Injection framework for Unity">
       <HomepageHeader />
       <main>
-	  <Highlight
-	  	img={
-                        <CodeBlock className="csharp" children={ReactIntegration}></CodeBlock>
-		}
-	  	isDark
-	  	title="Let UniDi glue.."
-	  	text={
+	<Section>
+	  <LogoCarousel logos={logos}></LogoCarousel>
+	</Section>
+	<Highlight
+	  img={
+		  <CodeBlock className="csharp" children={ReactIntegration}></CodeBlock>
+	  }
+	  isDark
+	  title="Let UniDi glue.."
+	  text={
+		  <>
+		  <p>
+		  Lorum ipsum
+		  </p>
+		  </>
+	  }
+	/>
+  	<Highlight
+	  img={
+                  <CodeBlock className="csharp" children={ReactIntegration}></CodeBlock>
+	  }
+	  reversed
+	  title="Let UniDi glue.."
+	  text={
 		  <>
 		  Lorum ipsum
 		  </>
-	  	}
-	  />
+	  }
+	/>
       </main>
     </Layout>
   );
