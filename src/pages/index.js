b5e1a306e94367b2a41b46b033e9911e5bd26f03
Brad Menchl <brad.menchl@onepeloton.com>
2022-01-22 16:40:42 -0500

    update home page and intro page

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
 	  <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/intro">
             Read the Docs 📚
           </Link>
 	  &nbsp;
